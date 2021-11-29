3f67dc0975def18a2271cc6ab1be57544b088ce9
KimBoris <zlas2345@gmail.com>
2021-11-30 02:48:30 +0900

    Cactus
    대용량 파일 LFS

---
+++ cactus/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using CreatorKitCode;
+
+public class SpawnerSample : MonoBehaviour
+{
+    public GameObject ObjectToSpawn;
+
+    void Start()
+    {
+        SpawnPotion(0);
+        SpawnPotion(45);
+        SpawnPotion(90);
+        SpawnPotion(135);
+    }
+
+    void SpawnPotion(int angle)
+    {
+        int radius = 5;
+        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        Vector3 spawnPosition = transform.position + direction * radius;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    }
+}
+
